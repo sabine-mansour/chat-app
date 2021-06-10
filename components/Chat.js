4df82bb9221ded84748c734c8dbb7ebcf30de271
sabine-mansour <sabine_mansour@icloud.com>
2021-06-10 19:18:04 +0200

    Updated Background Color to match props

--- components/Chat.js
+++ components/Chat.js
@@ -4,12 +4,19 @@ import { View, Text } from 'react-native';
 
 export default class Chat extends React.Component {
   render() {
-    let { name } = this.props.route.params;
+    let { name, backColor } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
 
+    let textColor = 'black';
+    if (backColor === '#090C08' || backColor === '#474056') {
+      textColor = 'white'
+    }
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Welcome to the chat!</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backColor }}>
+        <Text style={{ color: textColor }}>
+          Welcome to the chat!
+          </Text>
       </View>
     )
   }
